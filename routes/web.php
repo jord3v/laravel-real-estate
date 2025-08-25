1a5e3dc686e9a6189843adce5e73ef9d2ff08355
Jorge Miguel <jorge.ideapad3@outlook.com.br>
2025-08-25 09:58:07 -0300

    feat: implement multi-tenancy support with Stancl Tenancy package, add tenant and domain models, and configure routes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+foreach (config('tenancy.central_domains') as $domain) {
+        Route::domain($domain)->group(function () {
+            Route::get('/', function () {
+            return 'Aqui va la pagina principal';
+        });
+    });
+}
